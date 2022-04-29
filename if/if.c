cb0daffd1ad14669b56b0e97a649fbb81a04c097
hayun <102849597+hmogae@users.noreply.github.com>
2022-04-29 23:31:03 +0900

    Add files via upload

---
+++ if/if.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+  // 아래코드 참조
+}
+
+// 상, 중, 하 구하기
+//	int score;
+//	scanf("%d", &score);
+//	if (score >= 80)
+//		printf("상위 99.9%");
+//	else if (score >= 70)
+//		printf("중위 99.9%");
+//	else
+//		printf("하위 0.1%");
+
+// 점수(행운)
+//	int score;
+//	scanf("%d", &score);
+//	if (score >= 70 && score < 80)
+//		printf("성공!");
+//	else
+//		printf("꽝~!");
+//	return 0;
+
+// 큰가 작은가 구하기
+//	int number;
+//	scanf("%d", &n);
+//	if (n < 10)
+//		printf("작다.");
+//	else
+//		print("크다.");
+//	return 0;
+
+//	int number;
+//	scanf("%d", &n);
+//	if (n < 10)
+//		printf("작다.");
+//	return 0;
+
+// 윷놀이
+// 	int yut;
+//	scanf("%d", &yut);
+//	switch (yut) {
+//	case 0:
+//		printf("모"); break;
+//	case 1:
+//		printf("도"); break;
+//	case 2:
+//		printf("개"); break;
+//	case 3:
+//		printf("걸"); break;
+//	case 4:
+//		printf("윷"); break;
+//}
+
---
+++ if/langko.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+	int langko;
+	scanf("%d", &langko);
+	switch (langko) {
+	case 1:
+		printf("ㄱ"); break;
+	case 2:
+		printf("ㄴ"); break;
+	case 3:
+		printf("ㄷ"); break;
+	case 4:
+		printf("ㄹ"); break;
+	case 5:
+		printf("ㅁ"); break;
+	case 6:
+		printf("ㅂ"); break;
+	case 7:
+		printf("ㅅ"); break;
+	case 8:
+		printf("ㅇ"); break;
+	case 9:
+		printf("ㅈ"); break;
+	case 10:
+		printf("ㅊ"); break;
+	case 11:
+		printf("ㅋ"); break;
+	case 12:
+		printf("ㅍ"); break;
+	case 13:
+		printf("ㅌ"); break;
+	case 14:
+		printf("ㅎ"); break;
+	case 15:
+		printf("ㅏ"); break;
+	case 16:
+		printf("ㅑ"); break;
+	case 17:
+		printf("ㅓ"); break;
+	case 18:
+		printf("ㅕ"); break;
+	case 19:
+		printf("ㅗ"); break;
+	case 20:
+		printf("ㅛ"); break;
+	case 21:
+		printf("ㅜ"); break;
+	case 22:
+		printf("ㅠ"); break;
+	case 23:
+		printf("ㅡ"); break;
+	case 24:
+		printf("ㅣ"); break;
+	}
+}
